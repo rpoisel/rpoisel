332438197230684e067ee73528a405a795000030
Rainer Poisel <rainer@honeytreelabs.com>
2024-09-06 00:02:09 +0200

    feat: make debugging failing processes better

--- rpoisel/process.py
+++ rpoisel/process.py
@@ -1,13 +1,19 @@
 import subprocess
 
+import click
+
 
 def run_shell_check(args: str | list[str]) -> str:
-    completed_process = subprocess.run(
-        args,
-        check=True,
-        shell=True,
-        executable="/bin/bash",
-        capture_output=True,
-        text=True,
-    )
-    return completed_process.stdout
+    try:
+        completed_process = subprocess.run(
+            args,
+            check=True,
+            shell=True,
+            executable="/bin/bash",
+            capture_output=True,
+            text=True,
+        )
+        return completed_process.stdout
+    except subprocess.CalledProcessError as exc:
+        click.echo(f"Problem (returncode={exc.returncode}): {exc.stdout} {exc.stderr}")
+        raise
